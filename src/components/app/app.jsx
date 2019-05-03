a8ba2d153b205fc2df43a3a602a3edd5575f14e1
Николай <arcmag-work@mail.ru>
2019-05-03 14:41:20 +0700

    Выполняет шаги из задания - Личный проект: валидируй, как никогда не валидировал

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
-const App = () => {
-  return <h1>React application</h1>;
+import Favorites from '../favorites/favorites.jsx';
+
+const App = (props) => {
+  const rentList = props.rentList;
+
+  App.propTypes = {
+    rentList: propTypes.array
+  };
+
+  return <div>
+    <h1>React application</h1>
+    {rentList.map((it, idx) => <Favorites key={idx} name={it} />)}
+  </div>;
 };
 
 export default App;
