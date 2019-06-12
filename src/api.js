06175afae7270cea80a5e653373d1accff3383ec
Николай <arcmag-work@mail.ru>
2019-06-12 18:16:06 +0700

    Исправляет константы в api.js

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const timeout = 5000;
-const baseURL = `https://es31-server.appspot.com/six-cities`;
+const TIME_OUT = 5000;
+const BASE_URL = `https://es31-server.appspot.com/six-cities`;
 
 const STATUS_ERROR = 403;
 
 export const createAPI = (onLoginFail) => {
   const api = axios.create({
-    baseURL,
-    timeout,
+    baseURL: BASE_URL,
+    timeout: TIME_OUT,
     withCredentials: true,
   });
 
